83578d1f4cb8557ecdec3eabcf202965341308bd
dkurnikov <kurnikov.dmitrii@gmail.com>
2021-11-26 13:41:47 +0300

    a1

--- src/main/java/com/example/demo/Controller.java
+++ src/main/java/com/example/demo/Controller.java
@@ -5,14 +5,12 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
 import java.net.MalformedURLException;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.URL;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.stream.Collectors;
 
 @RestController
@@ -21,23 +19,31 @@ public class Controller {
     private static final HashMap<String, Integer> domains = new HashMap<String, Integer>();
     private static final List<String> url_list = new ArrayList<>();
 
-    @RequestMapping(value = "/add/{name}")
-    public List<String> add( @PathVariable String name) {
+    @RequestMapping(value = "/add/{name:[a-zA-Z0-9.]*}")
+    public List<String> add(@PathVariable String name) {
+        try {
+            String[] url = name.split("/");
+            url = url[0].split("\\.");
 
-        if (domains.containsKey(name))
-            domains.compute(name, (k, v) -> v = v + 1);
-        else
-            domains.put(name, 0);
+            String host = url[url.length - 2];
 
-        url_list.add(name);
+            if (domains.containsKey(host))
+                domains.compute(host, (k, v) -> v = v + 1);
+            else
+                domains.put(host, 0);
 
-        return url_list;
+            url_list.add(name);
+
+            return url_list;
+        }catch (ArrayIndexOutOfBoundsException e){
+            System.out.println(e);
+            return Arrays.asList("Wrong URL!");
+        }
     }
 
     @RequestMapping(value = "/top/{n}")
     public List<String> top(@PathVariable int n){
-
-        return domains.entrySet().stream().sorted(Map.Entry.comparingByValue())
+        return domains.entrySet().stream().sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
                 .map(Map.Entry::getKey).limit(n).collect(Collectors.toList());
     }
 
