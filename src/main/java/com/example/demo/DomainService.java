df4618c3e4e1c50f3791f4b1fb44d9ad30662120
dkurnikov <kurnikov.dmitrii@gmail.com>
2021-11-30 14:31:44 +0300

    refactoring

--- src/main/java/com/example/demo/DomainService.java
+++ src/main/java/com/example/demo/DomainService.java
@@ -8,32 +8,36 @@ import java.util.stream.Collectors;
 @Service
 public class DomainService {
 
-        private static final HashMap<String, Integer> domains = new HashMap<String, Integer>();
-        private static final List<String> url_list = new ArrayList<>();
+    private static final HashMap<String, Integer> domains = new HashMap<String, Integer>();
+    private static final List<String> urlList = new ArrayList<>();
 
-        public List<String> add(String url){
-                try {
-                        String[] host = url.split("\\.");
+    public List<String> add(String url) {
+        try {
+            String[] host = url.split("\\.");
 
-                        if (domains.containsKey(host[host.length - 2]))
-                                domains.compute(host[host.length - 2], (k, v) -> v = v + 1);
-                        else
-                                domains.put(host[host.length - 2], 1);
+            if (domains.containsKey(host[host.length - 2])) {
+                domains.compute(host[host.length - 2], (k, v) -> v = v + 1);
+            } else {
+                domains.put(host[host.length - 2], 1);
+            }
 
-                        url_list.add(url);
-                        return url_list;
-                }catch(ArrayIndexOutOfBoundsException e){
-                        return Arrays.asList("Wrong URL!");
-                }
+            urlList.add(url);
+            return urlList;
+        } catch (ArrayIndexOutOfBoundsException e) {
+            return Arrays.asList("Wrong URL!");
         }
+    }
 
-        public List<String> top(int n){
-                return domains.entrySet().stream().sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
-                        .map(Map.Entry::getKey).limit(n).collect(Collectors.toList());
-        }
+    public List<String> top(int n) {
+        return domains.entrySet().stream()
+                .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
+                .map(Map.Entry::getKey)
+                .limit(n)
+                .collect(Collectors.toList());
+    }
 
-        public HashMap<String, Integer> domain(){
-                return domains;
-        }
+    public HashMap<String, Integer> domain() {
+        return domains;
+    }
 
 }
