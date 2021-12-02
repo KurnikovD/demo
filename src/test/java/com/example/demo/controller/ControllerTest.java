1b6d8bfb6c24de2228491671db072e24d3bb90e0
dkurnikov <kurnikov.dmitrii@gmail.com>
2021-12-02 13:23:45 +0300

    Create test for Controller and Service

---
+++ src/test/java/com/example/demo/controller/ControllerTest.java
@@ -0,0 +1,78 @@
+package com.example.demo.controller;
+
+import com.example.demo.service.DomainService;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.Collections;
+import java.util.List;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.equalTo;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+class ControllerTest {
+
+    @Autowired
+    Controller controller;
+    @MockBean
+    DomainService service;
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void testCorrectTopEmpty() throws Exception {
+        Mockito.when(service.top(Mockito.anyInt())).thenReturn(Collections.emptyList());
+
+        this.mockMvc.perform(get("/top/1"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(equalTo("[]")));
+    }
+
+    @Test
+    public void testCorrectTopIsNotEmpty() throws Exception {
+        Mockito.when(service.top(Mockito.anyInt())).thenReturn(List.of("vk", "google"));
+
+        this.mockMvc.perform(get("/top/2"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(equalTo("[\"vk\",\"google\"]")));
+    }
+
+    @Test
+    void testAdd() throws Exception {
+        this.mockMvc.perform(get("/add/ya.ru/"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString("")));
+
+        Mockito.verify(service).add("ya.ru/");
+    }
+
+    @Test
+    void testFailedTopIfArgumentIsLetter() throws Exception {
+        this.mockMvc.perform(get("/top/a"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString("Error! Is not a number")));
+    }
+
+    @Test
+    void testFailedTopIfArgumentIsBellowThenZero() throws Exception {
+        this.mockMvc.perform(get("/top/-1"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString("Error! The number must be greater than 0")));
+    }
+
+
+
+}
---
+++ src/test/java/com/example/demo/service/DomainServiceImplTest.java
@@ -0,0 +1,52 @@
+package com.example.demo.service;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.Assert.assertThrows;
+
+
+class DomainServiceImplTest {
+
+    @Test
+    void testAddOk() {
+        DomainService domainService = new DomainServiceImpl();
+        domainService.add("google.com");
+        assert (domainService.domain().containsKey("google"));
+    }
+
+    @Test
+    void testAddException() {
+        DomainService domainService = new DomainServiceImpl();
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+            domainService.add("google");
+        });
+        String exceptionMessage = "Wrong URL!";
+        String actualMessage = exception.getMessage();
+        assert (actualMessage.contains(exceptionMessage));
+    }
+
+    @Test
+    void testReturnNResultsFromTop() {
+        DomainService domainService = new DomainServiceImpl();
+        domainService.add("google.com");
+        domainService.add("lenta.ru");
+        domainService.add("vk.com");
+        int n = 2;
+        assert (domainService.top(n).size() == n);
+    }
+
+    @Test
+    void testContentInList(){
+        DomainService domainService = new DomainServiceImpl();
+        domainService.add("google.com");
+        domainService.add("lenta.ru");
+        domainService.add("vk.com");
+
+        List<String> list = List.of("lenta", "vk", "google");
+        assert (list.containsAll(domainService.top(2)));
+    }
+
+
+}
