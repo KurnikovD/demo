e19cc36b495ec28adc2431c558ccb4e1e3b56e07
dkurnikov <kurnikov.dmitrii@gmail.com>
2021-12-03 12:52:14 +0300

    refactoring

--- src/main/java/com/example/demo/service/DomainServiceImpl.java
+++ src/main/java/com/example/demo/service/DomainServiceImpl.java
@@ -1,12 +1,8 @@
 package com.example.demo.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.CacheEvict;
-import org.springframework.cache.annotation.CachePut;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import java.util.Comparator;
